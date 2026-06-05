7f159d923b93004a2101585b8447357a0148b95d
SumitxThokar <sumitxthokar@gmail.com>
2026-06-05 06:12:39 +0000

    Refactor code structure for improved readability and maintainability

---
+++ src/fastapi_l/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from datetime import datetime
+
+class UserBase(BaseModel):
+    username: str= Field(min_length=1, max_length=30)
+    email: EmailStr = Field(max_length=120)
+    
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True, )
+    
+    id: int
+    image_file: str |None
+    image_path: str
+
+
+class PostBase(BaseModel):
+    title: str = Field(min_length=1, max_length=100)
+    content: str = Field(min_length=1)
+
+class PostCreate(PostBase):
+    user_id: int 
+
+class PostResponse(PostBase):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id: int
+    date_posted: datetime
+    user_id: int
+    author: UserResponse
